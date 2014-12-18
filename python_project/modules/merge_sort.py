68aef744a2d3dc21df15c613619f84935922e798
gaudi8 <gaudi8@gmail.com>
2014-12-18 11:50:36 +0900

    merge sort

--- python_project/modules/merge_sort.py
+++ python_project/modules/merge_sort.py
@@ -1,41 +1,51 @@
+# coding=utf-8
 __author__ = 'gaudi8'
 
 import random
+import math
 
-def merge_sort(n, list):
-  if n <= 1:
-    return
-
-  m = n / 2
-  merge_sort(m, list)
-  merge_sort(n - m, list + m)
-
-  for i in range(0, m):
-    buffer[i] = list[i]
-
-  j = m
-  i = k = 0
-
-  while i < m and j < n:
-    if buffer[i] <= list[j]:
-      list[k] = buffer[i]
-    else:
-      list[k] = list[j]
-    k += 1
-    i += 1
-
-  while i < m:
-    list[k] = buffer[i]
-
-
+# ソートするリストの要素数
 NUM = 10
-# 
+
+
+def merge_sort(n, given_list):
+  """
+
+  :rtype : list
+  """
+  if n <= 1:
+    return given_list
+
+  m = int(math.ceil(n / 2))
+  first_list = merge_sort(m, given_list[:m])
+  last_list = merge_sort(n - m, given_list[m:n])
+
+  return_list = []
+  while len(first_list) * len(last_list):
+    if first_list[0] <= last_list[0]:
+      return_list.append(first_list[0])
+      first_list.pop(0)
+    else:
+      return_list.append(last_list[0j])
+
+  if m < n:
+    return_list.append(last_list[-1])
+
+  return return_list
+
+
+# def test(list):
+# list = [1,2]
+# ソートするリスト
 sort = []
 
 for i in range(0, NUM):
-  sort[i] = random.randint(0, 100)
+  sort.append(random.randint(0, 100))
 
-merge_sort(NUM, sort)
+print(sort)
+sort = merge_sort(NUM, sort)
+# test(sort)
+print(sort)
 
 
 
